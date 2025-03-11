10c060786c20325f3f8e126221d88b2bde0857fc
Siddharth <siddharthanand7408@gmail.com>
2025-03-11 21:03:28 +0530

    fixed

--- src/main/java/com/test2/Test2Application.java
+++ src/main/java/com/test2/Test2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test2Application {
 
 	public static void main(String[] args) {
+int x=10;
 		SpringApplication.run(Test2Application.class, args);
 	}
 
